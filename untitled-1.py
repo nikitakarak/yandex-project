57532a81cf5596e0129f5405132188fb28acc2ca
ga_rodionov <ga_rodionov@mail.ru>
2020-03-15 14:49:19 +0300

    2

--- untitled-1.py
+++ untitled-1.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask import url_for
+import json
 
 app = Flask(__name__)
 
@@ -20,6 +21,14 @@ def training(prof):
     c = "Чертёжи"
     return render_template('odd_even.html', title='127.0.0.1:8080/training/{}'.format(prof),
                            u=a, u2=b, u3=c, prof=prof)
+
+
+@app.route('/list_prof/<list>')
+def news(list):
+    with open("news.json", "rt", encoding="utf8") as f:
+        news_list = json.loads(f.read())
+    print(news_list)
+    return render_template('news.html', news=news_list, lis=list)
 
 
 if __name__ == '__main__':
